c524286247a2c9e9354bfd7d76055129fa6bc1bc
Thorbjørn Hermansen <thhermansen@gmail.com>
2016-07-25 16:25:11 +0200

    Added an extra spec for the encoded policy.

--- spec/ink_file_picker/policy_spec.rb
+++ spec/ink_file_picker/policy_spec.rb
@@ -14,6 +14,16 @@ describe InkFilePicker::Policy do
   it { expect(subject.policy).to eq 'eyJleHBpcnkiOjEzOTQzNjM4OTYsImNhbGwiOiJyZWFkIn0=' }
   it { expect(subject.signature).to eq '4c50ca71d9e123274a01eb00a7facd52069e07c2e9312517f55bf1b94447792e' }
 
+  describe "the policy" do
+    let(:decoded) do
+      JSON.parse Base64.urlsafe_decode64 subject.policy
+    end
+
+    it { expect(decoded['call']).to eq 'read' }
+    it { expect(decoded['expiry']).to eq 1394363896 }
+
+  end
+
   describe "#to_hash" do
     it "contains policy and signature when secret is given" do
       expect(subject.to_hash).to eq({
